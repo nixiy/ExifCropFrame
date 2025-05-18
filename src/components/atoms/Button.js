2c4f7309abea0d72ad1dab34b22a738f4cca2b6f
nixiy <mavericks.dival.desire@gmail.com>
2025-05-18 21:16:22 +0900

    不要なボタンスタイルを削除し、ボタンコンポーネントのクラス名を簡素化

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -10,26 +10,10 @@ import React from 'react';
  * @returns {JSX.Element} - ボタンコンポーネント
  */
 const Button = ({ type = 'primary', onClick, disabled, children }) => {
-  const getClassName = () => {
-    const baseClass = 'button';
-    switch (type) {
-      case 'primary':
-        return `${baseClass} primary-button`;
-      case 'secondary':
-        return `${baseClass} secondary-button`;
-      case 'danger':
-        return `${baseClass} danger-button`;
-      case 'info':
-        return `${baseClass} info-button`;
-      case 'success':
-        return `${baseClass} success-button`;
-      default:
-        return `${baseClass} primary-button`;
-    }
-  };
+  const className = 'button primary-button';
 
   return (
-    <button className={getClassName()} onClick={onClick} disabled={disabled}>
+    <button className={className} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
